942f66ae4388b06bd9e6711edb35be21f7d04de4
jessy <jessymontano@hotmail.com>
2024-01-19 22:32:21 -0700

    arreglar bug

--- js/funciones.js
+++ js/funciones.js
@@ -1,107 +1,149 @@
-var reinas = 8;
+const inactivas = []; //array con celdas inactivas
+var reinas = 8; //reinas por colocar
 
-function colocarReina(celda) {
+function colocarReina(celda){
 
-    if (window.getComputedStyle(celda).backgroundImage == "none") {
+    tablero = document.getElementById("tablero"); 
+    renglon = celda.parentElement.rowIndex;
+    columna = celda.cellIndex;
+
+    //poner reina si la celda no esta inactiva y si hay menos de 8 reinas
+    if(!celda.classList.contains("reina") && !celda.classList.contains("inactivo") && (8 - reinas) < 8){
         
-        if ((8 - reinas) < 8) {
-            celda.style = "background-image: url(./img/reina.png); background-size: cover;";
-            reinas--;
-            var renglon = celda.parentElement.rowIndex;
-            var columna = celda.cellIndex;
-            var tablero = document.getElementById("tablero");
-            
-            for (let i = 0; i < 8; i++) {
-                if (columna != i) {
-                    tablero.rows[renglon].cells[i].removeAttribute("onclick");
-                    tablero.rows[renglon].cells[i].style.backgroundColor = "#ff0000";
-                }
-                if (renglon != i) {
-                    tablero.rows[i].cells[columna].removeAttribute("onclick");
-                    tablero.rows[i].cells[columna].style.backgroundColor = "#ff0000";
-                }
+        celda.classList.add("reina");
+        reinas--;
+
+        //desactivar renglon y columna
+        for (let i = 0; i < 8; i++) {
+
+            if(columna != i){
+                tablero.rows[renglon].cells[i].classList.add("inactivo");
+                inactivas.push(`${renglon} ${i}`);
             }
-            var r = renglon;
-            var c = columna;
-            while (r >= 0 && c < 8) {
-                tablero.rows[r].cells[c].style.backgroundColor = "#ff0000";
-                r--;
-                c++;
-            }
-
-            r = renglon + 1;
-            c = columna -1;
-
-            while (c >= 0 && r < 8) {
-                tablero.rows[r].cells[c].style.backgroundColor = "#ff0000";
-                r++;
-                c--;
-            }
-            r = renglon + 1;
-            c = columna + 1;
-
-            while (c < 8 && r < 8) {
-                tablero.rows[r].cells[c].style.backgroundColor = "#ff0000";
-                r++;
-                c++;
-            }
-
-            r = renglon -1;
-            c = columna -1;
-
-            while(c < 8 && r < 8){
-                tablero.rows[r].cells[c].style.backgroundColor = "#ff0000";
-                r--;
-                c--;
+            if(renglon != i){
+                tablero.rows[i].cells[columna].classList.add("inactivo");
+                inactivas.push(`${i} ${columna}`);
             }
         }
-    } else {
-        celda.style = "background-image: none;";
+        
+        //desactivar diagonales
+        var r = renglon - 1;
+        var c = columna + 1;
+        while (r >= 0 && c < 8) {
+            tablero.rows[r].cells[c].classList.add("inactivo");
+            inactivas.push(`${r} ${c}`);
+            r--;
+            c++;
+        }
+
+        r = renglon + 1;
+        c = columna -1;
+        while (c >= 0 && r < 8) {
+            tablero.rows[r].cells[c].classList.add("inactivo");
+            inactivas.push(`${r} ${c}`);
+            r++;
+            c--;
+        }
+
+        r = renglon + 1;
+        c = columna + 1;
+        while (c < 8 && c >= 0 && r < 8 && r >= 0) {
+            tablero.rows[r].cells[c].classList.add("inactivo");
+            inactivas.push(`${r} ${c}`);
+            r++;
+            c++;
+        }
+
+        r = renglon -1;
+        c = columna -1;
+        while(c < 8 && c >= 0 && r < 8 && r >= 0){
+            tablero.rows[r].cells[c].classList.add("inactivo");
+            inactivas.push(`${r} ${c}`);
+            r--;
+            c--;
+        }
+    }
+    //quitar reina
+    else{
+        celda.classList.remove("reina");
         reinas++;
-        var renglon = celda.parentElement.rowIndex;
-        var columna = celda.cellIndex;
-        var tablero = document.getElementById("tablero");
-            for (let i = 0; i < 8; i++) {
-                    tablero.rows[renglon].cells[i].setAttribute("onclick", "colocarReina(this)");
-                    tablero.rows[renglon].cells[i].style.backgroundColor = "";
-                    tablero.rows[i].cells[columna].setAttribute("onclick", "colocarReina(this)");
-                    tablero.rows[i].cells[columna].style.backgroundColor = "";
-            }
-            var r = renglon;
-            var c = columna;
-            while (r >= 0 && c < 8) {
-                tablero.rows[r].cells[c].style.backgroundColor = "";
-                r--;
-                c++;
-            }
 
-            r = renglon + 1;
-            c = columna -1;
-
-            while (c >= 0 && r < 8) {
-                tablero.rows[r].cells[c].style.backgroundColor = "";
-                r++;
-                c--;
+        //reactivar renglones y columnas
+        for (let i = 0; i < 8; i++) {
+            if (inactivas.includes(`${i} ${columna}`)) {
+                index = inactivas.indexOf(`${i} ${columna}`);
+                inactivas.splice(index, 1);
             }
-            r = renglon + 1;
-            c = columna + 1;
-
-            while (c < 8 && r < 8) {
-                tablero.rows[r].cells[c].style.backgroundColor = "";
-                r++;
-                c++;
+            if(!inactivas.includes(`${i} ${columna}`)) {
+                tablero.rows[i].cells[columna].classList.remove("inactivo");
             }
-
-            r = renglon -1;
-            c = columna -1;
-
-            while(c < 8 && r < 8){
-                tablero.rows[r].cells[c].style.backgroundColor = "";
-                r--;
-                c--;
+            if (inactivas.includes(`${renglon} ${i}`)) {
+                index = inactivas.indexOf(`${renglon} ${i}`);
+                inactivas.splice(index, 1);
             }
+            if(!inactivas.includes(`${renglon} ${i}`)) {
+                tablero.rows[renglon].cells[i].classList.remove("inactivo");
+            }
+        }
+
+        var r = renglon;
+        var c = columna;
+        while (r >= 0 && c < 8) {
+            if (inactivas.includes(`${r} ${c}`)) {
+                index = inactivas.indexOf(`${r} ${c}`);
+                inactivas.splice(index, 1)
+            }
+            if(!inactivas.includes(`${r} ${c}`)) {
+                tablero.rows[r].cells[c].classList.remove("inactivo");
+            }
+            r--;
+            c++;
+        }
+
+        r = renglon + 1;
+        c = columna -1;
+        while (c >= 0 && r < 8) {
+            if (inactivas.includes(`${r} ${c}`)) {
+                index = inactivas.indexOf(`${r} ${c}`);
+                inactivas.splice(index, 1)
+            }
+            if(!inactivas.includes(`${r} ${c}`)) {
+                tablero.rows[r].cells[c].classList.remove("inactivo");
+            }
+            r++;
+            c--;
+        }
+
+        r = renglon + 1;
+        c = columna + 1;
+        while (c < 8 && c >= 0 && r < 8 && r >= 0) {
+            if (inactivas.includes(`${r} ${c}`)) {
+                index = inactivas.indexOf(`${r} ${c}`);
+                inactivas.splice(index, 1)
+            }
+            if(!inactivas.includes(`${r} ${c}`)) {
+                tablero.rows[r].cells[c].classList.remove("inactivo");
+            }
+            r++;
+            c++;
+        }
+
+        r = renglon -1;
+        c = columna -1;
+        while(c < 8 && c >= 0 && r < 8 && r >= 0){
+            if (inactivas.includes(`${r} ${c}`)) {
+                index = inactivas.indexOf(`${r} ${c}`);
+                inactivas.splice(index, 1)
+            }
+            if(!inactivas.includes(`${r} ${c}`)) {
+                tablero.rows[r].cells[c].classList.remove("inactivo");
+            }
+            r--;
+            c--;
+        }
     }
 
+    //actualizar contador de reinas
     document.getElementById("textoReinasPorColocar").innerHTML = "Reinas por colocar: " + reinas;
     document.getElementById("textoReinasColocadas").innerHTML = "Reinas colocadas: " + (8 - reinas);
 }
